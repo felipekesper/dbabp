53674b288024bb44ecb0cf2a2ec398a981fc9d1f
felipekesper <felipe.kesper1@hotmail.com>
2022-09-30 18:15:20 -0300

    autoinsert2

--- auto_insert.sh
+++ auto_insert.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+# Declaração de variáveis
 export ORACLE_HOME='/u01/app/oracle/database'
 export PATH='/u01/app/oracle/database/bin:/u01/app/oracle/database/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin'
 export ORACLE_SID='prod'
 
+# Número aleatório entre 1 e 1000 utilizado para gerar dados.
 i=`shuf -i 1-1000 -n 1`
 
+# Arquivo com informação da quantidade de dados inseridos
 echo `date '+%d_%m_%Y-%H_%M'`_${i} >> /home/oracle/qtinserted.txt
 
 echo "Downloading values..."
 
+# Web API utilizado para gerar dados aleatórios.
 urlp="https://api.mockaroo.com/api/e89b45b0?count=${i}&key=ab17d350"
 urlc="https://api.mockaroo.com/api/25696190?count=${i}&key=ab17d350"
 urlj="https://api.mockaroo.com/api/277eea10?count=${i}&key=ab17d350"
 
+# Arquivos com dados inseridos
 filep=people_`date '+%d_%m_%Y-%H_%M'`.sql
 filec=cars_`date '+%d_%m_%Y-%H_%M'`.sql
 filej=jobs_`date '+%d_%m_%Y-%H_%M'`.sql
@@ -21,9 +26,6 @@ filej=jobs_`date '+%d_%m_%Y-%H_%M'`.sql
 curl -s $urlp > "/home/oracle/people/${filep}"
 curl -s $urlc > "/home/oracle/cars/${filec}"
 curl -s $urlj > "/home/oracle/jobs/${filej}"
-
-#@/home/oracle/cars/${filec}
-#@/home/oracle/people/${filep}
 
 /u01/app/oracle/database/bin/sqlplus -S felipe/password << HERE	
 	!echo "Inserting into people2..."
